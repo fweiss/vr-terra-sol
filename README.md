af2f1e88d0f3dac10b0427a2d88f42105f39defd
Frank E Weiss <feweiss@gmail.com>
2022-10-03 08:36:10 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# vr-terra-sol
+Visualization of the spatial relationship between earth and sun
+
+## Links and references
+https://reddotblues.medium.com/babylon-js-typescript-project-setup-for-the-impatient-d8c71b4a57ad
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./src/app.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.ts', 'tsx', '.js']
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'assets', to: 'assets' },              
+            ],
+        }),
+    ],
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    }
+}
