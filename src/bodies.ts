6070707913383537852d8be01e5f30428476e516
Frank E Weiss <feweiss@gmail.com>
2023-06-13 09:34:33 -0700

    Extract create sun and earth

--- src/bodies.ts
+++ src/bodies.ts
@@ -6,6 +6,23 @@ export default class Bodies {
     earth: BABYLON.Mesh
 
     constructor(scene: BABYLON.Scene, universe: BABYLON.AbstractMesh) {
+        const starfield = createStarfield(scene, universe)
+        this.createSun(scene, universe)
+        this.createEarth(scene, universe)
+ 
+        // todo move to lights
+        const sunlight = new BABYLON.DirectionalLight('sunlight', new BABYLON.Vector3(1, 0, 0), scene)
+        sunlight.excludedMeshes =  [ starfield ]
+        sunlight.intensity = 5
+        sunlight.specular = new BABYLON.Color3()
+    }
+    setEarth(tod) {
+        // because texture.uOffset doens't work
+        const textureAdjust = Math.PI / 2
+        const beta = (tod / 24) * Math.PI * 2 + textureAdjust
+        this.earth.rotation = new BABYLON.Vector3(0, beta, 0)
+    }
+    private createEarth(scene: BABYLON.Scene, universe: BABYLON.AbstractMesh) {
         let earth = BABYLON.MeshBuilder.CreateSphere("earth", { diameter: settings.earth.diameter, sideOrientation: BABYLON.Mesh.FRONTSIDE}, scene)
         universe.addChild(earth)
         this.earth = earth
@@ -19,14 +36,14 @@ export default class Bodies {
         const texture: BABYLON.Texture = new BABYLON.Texture(url, scene, noMipmapOrOptions, invertY)
         texture.uScale = -1.0 // since texture wraps backwards
         material.diffuseTexture = texture
-        // since texture is centered where spherical phi = -90 degrees or one quarter turn
+
+        // since texture is centered at spherical phi = -90 degrees or one quarter turn
         const alignSphericalTexture = 0.25
         texture.uOffset = alignSphericalTexture
 
         earth.material = material
-    
-        const starfield = createStarfield(scene, universe)
-
+    }
+    private createSun(scene: BABYLON.Scene, universe: BABYLON.AbstractMesh) {
         // dimensions in km
         // scaled down to make it visible, even though 1/2 degree of arc should scale
         // make the dimesions not-to-scale to emphasize the sun in the view
@@ -39,28 +56,18 @@ export default class Bodies {
         // myMaterial.emissiveColor = BABYLON.Color3.FromHexString('#ff2020') // make it stand out
         sun.material = myMaterial
         universe.addChild(sun)
-    
-        // todo move to lights
-        const sunlight = new BABYLON.DirectionalLight('sunlight', new BABYLON.Vector3(1, 0, 0), scene)
-        sunlight.excludedMeshes =  [ starfield ]
-        sunlight.intensity = 5
-        sunlight.specular = new BABYLON.Color3()
-    }
-    setEarth(tod) {
-        // because texture.uOffset doens't work
-        const textureAdjust = Math.PI / 2
-        const beta = (tod / 24) * Math.PI * 2 + textureAdjust
-        this.earth.rotation = new BABYLON.Vector3(0, beta, 0)
+
     }
 }
 
 function createStarfield(scene, universe: BABYLON.AbstractMesh) {
     const diameter = 1000000
-    let starfield = BABYLON.MeshBuilder.CreateSphere("starfield-sphere", { diameter: 1000000, sideOrientation: BABYLON.Mesh.BACKSIDE }, scene)
+    let starfield = BABYLON.MeshBuilder.CreateSphere("starfield", { diameter: 1000000, sideOrientation: BABYLON.Mesh.BACKSIDE }, scene)
     starfield.rotate(new BABYLON.Vector3(1, 0, 0), Math.PI)
     let material = new BABYLON.StandardMaterial('stars', scene)
     material.diffuseTexture = new BABYLON.Texture('assets/starfield.jpg', scene)
     starfield.material = material
+    universe.addChild(starfield)
     return starfield
 }
 
