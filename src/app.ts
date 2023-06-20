c62a7b08e70b7affedb3ed745c034d8df7f3ae28
Frank E Weiss <feweiss@gmail.com>
2023-06-20 14:26:21 -0700

    WIP extract methods

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,25 @@ import Camera from './camera'
 import Bodies from './bodies'
 
 export default class App {
+    // should be const or private, but BABYLONJS makes it global anyway
+    scene: BABYLON.Scene
+    
     constructor() {
-        const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement
-        const engine = new BABYLON.Engine(canvas, true)
-        const scene: BABYLON.Scene = new BABYLON.Scene(engine)
-        engine.runRenderLoop(function () {
-            scene.render()
-        });
-        window.addEventListener("resize", function () {
-            engine.resize()
-        });
+        this.createRenderLoop()
         
         // put it all in a scalable mesh, trying to cope with crappy rendering of the sun at great distance
         const universe = new BABYLON.AbstractMesh('universe')
         const scaling = 0.001
         universe.scaling = new BABYLON.Vector3(scaling, scaling, scaling)
         
-        createLights(universe)
-        const camera = new Camera(scene, universe)
-        const bodies = new Bodies(scene, universe)
+        this.createLights(universe)
+        const camera = new Camera(this.scene, universe)
+        const bodies = new Bodies(this.scene, universe)
         
         const controls: Controls = new Controls()
         controls.target.addEventListener('camera', (event: CustomEvent) => {
             const cameraName = event.detail
-            camera.switchCamera(cameraName, scene, universe)
+            camera.switchCamera(cameraName, this.scene, universe)
         })
         controls.target.addEventListener('tod', (event: CustomEvent) => {
             console.log(event.detail)
@@ -37,28 +32,8 @@ export default class App {
         })
         bodies.setEarth(0)
         
-        debugmodes()
-        
-        function createLights(universe: BABYLON.AbstractMesh) {
-            // use two oposing hemisphere lights to provide full ambient
-            const intensity = .5
-            let light = new BABYLON.HemisphericLight("hemilight_north", new BABYLON.Vector3(0, 100000, 0), scene)
-            light.diffuse = new BABYLON.Color3(intensity, intensity, intensity)
-            light.specular = new BABYLON.Color3(0, 0, 0);
-        
-            let light2 = new BABYLON.HemisphericLight("hemilight_south", new BABYLON.Vector3(0, -100000, 0), scene)
-            light2.diffuse = new BABYLON.Color3(intensity, intensity, intensity)
-            light2.specular = new BABYLON.Color3(0, 0, 0);
-        }
-        
-        function debugmodes() {
-            if (settings.debug.inspector) {
-                scene.debugLayer.show()
-            }
-            if (settings.debug.axesViewer) {
-                new BABYLON.AxesViewer(scene, 2000)
-            }    
-        }
+        this.debugmodes()
+                
         camera.onCameraChangeObservable.add((cameraName: string) => {
             const latitude = camera.orbitSpherical.theta / Math.PI * 180 - 90
             let longitude = camera.orbitSpherical.phi / Math.PI * 180
@@ -74,4 +49,38 @@ export default class App {
             lonElem.value = longitude.toFixed(4).toString()
         })
     }
+    createRenderLoop() {
+        const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement
+        const engine = new BABYLON.Engine(canvas, true)
+
+        const scene: BABYLON.Scene = new BABYLON.Scene(engine)
+        engine.runRenderLoop(function () {
+            scene.render()
+        });
+        window.addEventListener("resize", function () {
+            engine.resize()
+        });
+        this.scene = scene
+    }
+    createLights(universe: BABYLON.AbstractMesh) {
+        // use two oposing hemisphere lights to provide full ambient
+        const intensity = .5
+        let light = new BABYLON.HemisphericLight("hemilight_north", new BABYLON.Vector3(0, 100000, 0), this.scene)
+        light.diffuse = new BABYLON.Color3(intensity, intensity, intensity)
+        light.specular = new BABYLON.Color3(0, 0, 0);
+    
+        let light2 = new BABYLON.HemisphericLight("hemilight_south", new BABYLON.Vector3(0, -100000, 0), this.scene)
+        light2.diffuse = new BABYLON.Color3(intensity, intensity, intensity)
+        light2.specular = new BABYLON.Color3(0, 0, 0);
+    }
+    debugmodes() {
+        if (settings.debug.inspector) {
+            this.scene.debugLayer.show()
+        }
+        if (settings.debug.axesViewer) {
+            new BABYLON.AxesViewer(this.scene, 2000)
+        }    
+    }
+
+
 }
