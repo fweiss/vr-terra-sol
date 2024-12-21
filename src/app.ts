653aa32fc68a115fffac134e8f600ade6ec11d3d
Frank Weiss <feweiss@gmail.com>
2024-12-21 15:30:30 -0800

    earth no clouds texture

--- src/app.ts
+++ src/app.ts
@@ -61,9 +61,22 @@ export default class App extends AppBase {
         paleBlueMaterial.diffuseColor = new BABYLON.Color3(0.68, 0.85, 0.9); // Pale blue color
         paleBlueMaterial.specularColor = BABYLON.Color3.Black()
 
+        let material = new BABYLON.StandardMaterial('earth_no_clouds', scene)
+        let res = '8k'
+        const url = 'assets/' + res + '/2_no_clouds_' + res + '.jpg'
+        const noMipmapOrOptions = false
+        const invertY = false // since default is oddly, true
+        const texture: BABYLON.Texture = new BABYLON.Texture(url, scene, noMipmapOrOptions, invertY)
+        texture.uScale = -1.0 // since texture wraps backwards
+        const alignSphericalTexture = 0.25
+        texture.uOffset = alignSphericalTexture
+        material.diffuseTexture = texture
+        material.specularColor = BABYLON.Color3.Black()
+
+
         // Create a sphere and apply the material
         this.earth = BABYLON.MeshBuilder.CreateSphere("earth", { diameter: 2 }, scene);
-        this.earth.material = paleBlueMaterial;
+        this.earth.material = material;
         this.earth.position = new BABYLON.Vector3(5, 0, 0);
 
         let phi = Math.PI / 2
